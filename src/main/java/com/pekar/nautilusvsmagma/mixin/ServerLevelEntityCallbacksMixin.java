3fca0f4d2811a22fb1d4923aa134258b08198019
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-05-30 17:40:08 +0200

    Migrate Fabric mod to Minecraft 1.21.11

--- src/main/java/com/pekar/nautilusvsmagma/mixin/ServerLevelEntityCallbacksMixin.java
+++ src/main/java/com/pekar/nautilusvsmagma/mixin/ServerLevelEntityCallbacksMixin.java
@@ -2,11 +2,8 @@ package com.pekar.nautilusvsmagma.mixin;
 
 import com.pekar.nautilusvsmagma.events.AnimalManager;
 import com.pekar.nautilusvsmagma.events.EntityLeaveLevelEvent;
-import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.entity.Entity;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,13 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(targets = "net.minecraft.server.level.ServerLevel$EntityCallbacks")
 public class ServerLevelEntityCallbacksMixin
 {
-    @Shadow
-    @Final
-    private ServerLevel this$0;
-
     @Inject(method = "onTrackingEnd(Lnet/minecraft/world/entity/Entity;)V", at = @At("HEAD"))
     private void nautilusvsmagma$onTrackingEnd(Entity entity, CallbackInfo ci)
     {
-        AnimalManager.onEntityLeaveLevelEvent(new EntityLeaveLevelEvent(entity, this$0));
+        AnimalManager.onEntityLeaveLevelEvent(new EntityLeaveLevelEvent(entity, entity.level()));
     }
 }
