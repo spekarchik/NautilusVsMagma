e1196c10e74b1fde660ed51c89968664bec50830
PEKAR\Sergey <sergey.pekarchik@gmail.com>
2026-06-19 15:52:07 +0200

    Added an in-game config screen accessible via **Mods → Nautilus vs Magma → Config**.
    (cherry picked from commit f5e7ba7563a73651a45c89e3c231ed38c327baa3)

---
+++ src/main/java/com/pekar/nautilusvsmagma/ClientMain.java
@@ -0,0 +1,28 @@
+package com.pekar.nautilusvsmagma;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.gui.ConfigurationScreen;
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
+
+@Mod(value = Main.MODID, dist = Dist.CLIENT)
+@EventBusSubscriber(modid = Main.MODID, value = Dist.CLIENT)
+public class ClientMain
+{
+    public ClientMain(ModContainer container)
+    {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+        container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event)
+    {
+    }
+}
